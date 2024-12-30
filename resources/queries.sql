bdbd1a2cee47d735ea89e4e1ec670b5651a4e27f
Jan Eike Suchard <jan@suchard.cloud>
2024-12-30 11:25:54 +0100

    adding query to get single article either by name or by id

--- resources/queries.sql
+++ resources/queries.sql
@@ -3,3 +3,6 @@ SELECT * FROM register.ticket_types;
 
 -- name: get-articles
 SELECT * FROM register.articles;
+
+-- name: get-article
+SELECT * FROM register.articles WHERE id = $1::uuid OR name = $1::text;
