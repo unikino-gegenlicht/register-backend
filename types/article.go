74a9da6f3ffc7cd379dcff9881a03e24431bd145
Jan Eike Suchard <jan@suchard.cloud>
2024-12-30 11:01:55 +0100

    fixed naming issue with tag

--- types/article.go
+++ types/article.go
@@ -5,8 +5,8 @@ type Article struct {
 	Name    string `db:"name"    json:"name"`
 	Enabled bool   `db:"enabled" json:"enabled"`
 	Prices  struct {
-		Members *float64 `db:"pice_members" json:"members"`
-		Guests  float64  `db:"price_guests" json:"guests"`
+		Members *float64 `db:"price_members" json:"members"`
+		Guests  float64  `db:"price_guests"  json:"guests"`
 	} `json:"prices" db:""`
 	Color string `db:"color" json:"color"`
 }
