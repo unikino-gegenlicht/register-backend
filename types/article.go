32ad131d66db2084a2bebd3657bff50b70e7cf49
Jan Eike Suchard <jan@suchard.cloud>
2024-12-31 09:48:58 +0100

    updating article struct tags

--- types/article.go
+++ types/article.go
@@ -1,12 +1,12 @@
 package types
 
 type Article struct {
-	ID      string `binding:"optional" db:"id"      json:"id"`
-	Name    string `binding:"required" db:"name"    json:"name"`
-	Enabled bool   `binding:"enabled"  db:"enabled" json:"enabled"`
+	ID      string `db:"id"            json:"id"`
+	Name    string `binding:"required" db:"name"      json:"name"`
+	Enabled bool   `db:"enabled"       json:"enabled"`
 	Prices  struct {
-		Members *float64 `binding:"required" db:"price_members" json:"members"`
-		Guests  float64  `binding:"required" db:"price_guests"  json:"guests"`
+		Members *float64 `db:"price_members" json:"members"`
+		Guests  float64  `binding:"required" db:"price_guests" json:"guests"`
 	} `json:"prices" db:""`
-	Color string `binding:"iscolor,default=#00a2ff" db:"color" json:"color"`
+	Color string `binding:"omitempty,hexcolor|rgb|hsl" db:"color" json:"color"`
 }
